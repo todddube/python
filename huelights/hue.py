102aa65dbe99b6ba045b1f28f84a2d293d59f1be
Todd Dube <todddube@users.noreply.github.com>
2024-04-21 09:43:18 -0400

    hue

---
+++ huelights/hue.py
@@ -0,0 +1,27 @@
+from huesdk import Hue
+import urllib3
+
+# disbale https warnings
+urllib3.disable_warnings()
+
+bridgeIP = '192.168.7.213'
+bridgeUsername = 'DEjNs62IugavQFib6aA4-s7RFVJPxfpxtAgGV1wm'
+
+if bridgeUsername == "":
+    username = Hue.connect(bridge_ip=bridgeIP, username=bridgeUsername)
+    print(username)
+ 
+hue = Hue(bridge_ip=bridgeIP, username=bridgeUsername)
+
+# Get all the lights connected to the bridge
+lights = hue.get_lights()
+for light in lights:
+    print(light.name)
+    #print(hue.get_light(x))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# get a single light with id
+print(hue.get_light(id_=2))
+
+# get light with name
+#light = hue.get_light(name="Room 1")
---
+++ huelights/readme.md
@@ -0,0 +1,11 @@
+# hue SDK
+- pip install huesdk
+- https://discovery.meethue.com/  get IP of huebridge
+- [
+    {
+        "id": "ecb5fafffea43228",
+        "internalipaddress": "192.168.7.213",
+        "port": 443
+    }
+]
+- https://pypi.org/project/huesdk/
