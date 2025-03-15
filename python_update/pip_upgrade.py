d45288a18f7369dddcb0c855806a8283f56532f8
Todd Dube <todddube@users.noreply.github.com>
2025-03-15 15:36:49 -0400

    updates

--- python_update/pip_upgrade.py
+++ python_update/pip_upgrade.py
@@ -1,6 +1,8 @@
+import datetime
 import subprocess
+import os
 import sys
-from datetime import date 
+from datetime import datetime  # Correct import
 
 def list_installed_packages():
     result = subprocess.run([sys.executable, '-m', 'pip', 'list', '--outdated', '--format=columns'], stdout=subprocess.PIPE)
@@ -22,30 +24,36 @@ def main():
     Purpose:    Upgrade all outdated pip packages and 
                 makes dates requirements backup dated. 
     Written by: Todd Dube
-    Date:       2025-03-09                                                                                                   
-    Version:    1.0.1
+    Date:       2025-03-15                                                                                                 
+    Version:    1.0.2
     ========================================================================
     """)
+    # getting current path 
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    # print(f"Current script path: {current_path}")
+    
+    # List outdated packages
     print("Listing outdated packages...")
     outdated_packages = list_installed_packages()
     
-    # write outdated packages to a file to use as a reference
-    today = date.today().strftime('%Y-%m-%d')
-    with open(f'outdated_packages_{today}.txt', 'w') as file:
+    # Write outdated packages to a file to use as a reference
+    today = datetime.now().strftime('%Y-%m-%d')  # Correct usage
+    output_file = current_path + f'/output/outdated_packages_{today}_{datetime.now().strftime("%H-%M-%S")}.txt'
+    print(f"Writing outdated packages to {output_file}")
+    with open(os.path.join(output_file), 'w') as file:
         file.write(outdated_packages)
-        subprocess.run(['code', f'outdated_packages_{today}.txt'])
-    
-    # Ask for confirmation before proceeding
-    response = input("Do you want to proceed with upgrading packages? (y/n): ").lower()
-    if response != 'y':
-        print("Upgrade cancelled.")
-        sys.exit(0)
 
-    requirements_file = f"requirements_{today}.txt"
+    # Write requirements to a file
+    requirements_file = current_path + f'/requirements_{today}_{datetime.now().strftime("%H-%M-%S")}.txt'
     with open(requirements_file, 'w') as file:
         subprocess.run([sys.executable, '-m', 'pip', 'freeze'], stdout=file)
     print(f"Requirements have been frozen to {requirements_file}")
- 
+
+    # Ask for confirmation before proceeding
+    response = input("Do you want to proceed with upgrading packages? (y/n): ").lower()
+    if response != 'y':
+        print("Upgrade cancelled....")
+        sys.exit(0)
 
     if outdated_packages:
         for line in outdated_packages.splitlines()[2:]:  # Skip header lines
